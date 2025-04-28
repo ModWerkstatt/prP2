93718be0c0244cddbf60d3ab555de51e82e8fe6b
Florian Thiel <flth@flth.de>
2025-04-28 23:52:17 +0200

    Konverterdatei gelöscht

--- res/textures/models/vehicle/train/pr_p2/batch.sh
+++
@@ -1,7 +0,0 @@
-for IMG in *.png;
-do
-	convert -format dds -define dds:compression=dxt1 -flip "$IMG" "$(basename "$IMG" .png).dds"
-#	convert $IMG -auto-orient png/"$(basename "$IMG" .tga).png";
-#    convert $IMG -auto-orient -resize 300x300\> $IMG2;
-#	convert $IMG -auto-orient -resize 300x300\> png/"$(basename "$IMG" .tga)_300.png";
-done
