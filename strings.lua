c90554b24efb1a65a08efc562f1628624fac6235
Florian Thiel <flth@flth.de>
2025-05-03 19:18:42 +0200

    KPEV Version vom Wagen hinzugefügt

--- strings.lua
+++ strings.lua
@@ -7,8 +7,9 @@ return {
 
             ["pr_p2_name"] = "Rh.E. P 2 (1879)",
 			["pr_p2_desc"] = "Für den Verkehr auf der kurvenreichen Strecke Köln-Trier beschaffte die Rheinische Eisenbahn-Gesellschaft für den Einsatz im Schnellzug- und Personenzugdienst sieben Lokomotiven mit der Achsformel 2'B, die auch die ersten Lokomotiven dieser Bauart in Preußen waren. Durch das bewegliche vordere Drehgestell war der Verschleiß der Spurkränze nicht so stark wie bei den Lokomotiven mit starrer Laufachse.",
-            ["reg_ab_name"] = "Rh.E. AB-Wagen",
+            ["reg_ab_name"] = "Rh.E. AB-Wagen (1879)",
             ["reg_ab_desc"] = "Für den Verkehr von und nach Köln beschaffte die Rheinische Eisenbahngesellschaft Abteilwagen mit gemischtklassigen Abteilen der 1. und 2. Klasse.",
+            ["kpev_ab_name"] = "K.P.E.V. AB-Wagen (1881)",
 
 		},
 		en = {
@@ -18,8 +19,9 @@ return {
 
 			["pr_p2_name"] = "Rh.E. P 2 (1879)",
 			["pr_p2_desc"] = "For traffic on the winding Cologne-Trier route, the Rheinische Eisenbahn-Gesellschaft procured seven locomotives with the 2'B axle formula for use in express and passenger train services, which were also the first locomotives of this design in Prussia. The movable front bogie meant that the wheel flanges did not wear as much as on locomotives with a fixed running axle.",
-            ["reg_ab_name"] = "Rh.E. 1st/2nd cl. Coach",
+            ["reg_ab_name"] = "Rh.E. 1st/2nd cl. Coach (1879)",
             ["reg_ab_desc"] = "The Rheinische Eisenbahngesellschaft procured compartment coaches with mixed 1st and 2nd class compartments for transport to and from Cologne.",
+            ["kpev_ab_name"] = "K.P.E.V 1st/2nd cl. Coach (1881)",
 
 		},
 	}
