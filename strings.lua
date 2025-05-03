108b67194af39348860da7b9f0444409e0f55fe5
Florian Thiel <flth@flth.de>
2025-05-03 19:01:12 +0200

    Strings überarbeitet

--- strings.lua
+++ strings.lua
@@ -2,20 +2,24 @@ function data()
 return {
 		de = {
 
-			["mod_name"] = "Preußische P 2",
-			["mod_desc"] = "Für den Verkehr auf der kurvenreichen Strecke Köln-Trier beschaffte die Rheinische Eisenbahn-Gesellschaft für den Einsatz im Schnellzug- und Personenzugdienst sieben Lokomotiven mit der Achsformel 2'B, die auch die ersten Lokomotiven dieser Bauart in Preußen waren. Durch das bewegliche vordere Drehgestell war der Verschleiß der Spurkränze nicht so stark wie bei den Lokomotiven mit starrer Laufachse.",
+			["mod_name"] = "Rheinische Eisenbahngesellschaft Set",
+			["mod_desc"] = "Für den Verkehr auf der kurvenreichen Strecke Köln-Trier beschaffte die Rheinische Eisenbahn-Gesellschaft für den Einsatz im Schnellzug- und Personenzugdienst sieben Lokomotiven mit der Achsformel 2'B, die auch die ersten Lokomotiven dieser Bauart in Preußen waren. Durch das bewegliche vordere Drehgestell war der Verschleiß der Spurkränze nicht so stark wie bei den Lokomotiven mit starrer Laufachse.\nFür den Verkehr von und nach Köln beschaffte die Rheinische Eisenbahngesellschaft Abteilwagen mit gemischtklassigen Abteilen der 1. und 2.",
 
-            ["pr_p2_name"] = "Preußische P 2 (1879)",
+            ["pr_p2_name"] = "Rh.E. P 2 (1879)",
 			["pr_p2_desc"] = "Für den Verkehr auf der kurvenreichen Strecke Köln-Trier beschaffte die Rheinische Eisenbahn-Gesellschaft für den Einsatz im Schnellzug- und Personenzugdienst sieben Lokomotiven mit der Achsformel 2'B, die auch die ersten Lokomotiven dieser Bauart in Preußen waren. Durch das bewegliche vordere Drehgestell war der Verschleiß der Spurkränze nicht so stark wie bei den Lokomotiven mit starrer Laufachse.",
+            ["reg_ab_name"] = "Rh.E. AB-Wagen",
+            ["reg_ab_desc"] = "Für den Verkehr von und nach Köln beschaffte die Rheinische Eisenbahngesellschaft Abteilwagen mit gemischtklassigen Abteilen der 1. und 2. Klasse.",
 
 		},
 		en = {
 
-			["mod_name"] = "Prussian P 2",
+			["mod_name"] = "Rheinische Eisenbahngesellschaft Set",
 			["mod_desc"] = "For traffic on the winding Cologne-Trier route, the Rheinische Eisenbahn-Gesellschaft procured seven locomotives with the 2'B axle formula for use in express and passenger train services, which were also the first locomotives of this design in Prussia. The movable front bogie meant that the wheel flanges did not wear as much as on locomotives with a fixed running axle.",
 
-			["pr_p2_name"] = "Prussian P 2 (1879)",
+			["pr_p2_name"] = "Rh.E. P 2 (1879)",
 			["pr_p2_desc"] = "For traffic on the winding Cologne-Trier route, the Rheinische Eisenbahn-Gesellschaft procured seven locomotives with the 2'B axle formula for use in express and passenger train services, which were also the first locomotives of this design in Prussia. The movable front bogie meant that the wheel flanges did not wear as much as on locomotives with a fixed running axle.",
+            ["reg_ab_name"] = "Rh.E. 1st/2nd cl. Coach",
+            ["reg_ab_desc"] = "The Rheinische Eisenbahngesellschaft procured compartment coaches with mixed 1st and 2nd class compartments for transport to and from Cologne.",
 
 		},
 	}
